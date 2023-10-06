d8029ce0e3dea4866d63c9005b1ab11b1a5b789b
zer0.k <zer0kams@gmail.com>
2023-10-06 03:10:35 +0200

    HUD namespace fix for hud.cpp

--- kz/hud.cpp
+++ kz/hud.cpp
@@ -2,7 +2,7 @@
 #include "utils/utils.h"
 #include "movement/movement.h"
 
-void KZ::hud::OnProcessUsercmds_Post(CPlayerSlot &slot, bf_read *buf, int numcmds, bool ignore, bool paused)
+void KZ::HUD::OnProcessUsercmds_Post(CPlayerSlot &slot, bf_read *buf, int numcmds, bool ignore, bool paused)
 {
 	MovementPlayer *player = g_pPlayerManager->ToPlayer(slot);
 	if (!player->GetController()) return;
