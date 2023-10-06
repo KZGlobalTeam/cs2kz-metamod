5c4a21b12692ace532333878ba8dfe205cc1da3b
GameChaos <franzkits@gmail.com>
2023-10-06 19:45:48 +0300

    Add shortened typedefs for integer and float types
    + some defines for the static keyword

--- common.h
+++ common.h
@@ -19,3 +19,22 @@ extern CEntitySystem *g_pEntitySystem;
 #endif
 
 PLUGIN_GLOBALVARS();
+
+// these are for searchability, because static behaves differently in different scopes.
+#define internal static // static functions
+#define local_persist static // static variables
+#define global_var static // global variables
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef i32 b32; // 32 bit boolean
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef float f32;
+typedef double f64;
