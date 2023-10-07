44f255c3cd3a869841176083007b546c97bfebe5
zer0.k <zer0kams@gmail.com>
2023-10-07 14:35:33 +0200

    Add destructor to CMovementPlayerManager

--- src/movement/movement.h
+++ src/movement/movement.h
@@ -89,7 +89,13 @@ public:
 			players[i] = new MovementPlayer(i);
 		}
 	}
-
+	~CMovementPlayerManager()
+	{
+		for (int i = 0; i < MAXPLAYERS + 1; i++)
+		{
+			delete players[i];
+		}
+	}
 public:
 	MovementPlayer *ToPlayer(CCSPlayer_MovementServices *ms);
 	MovementPlayer *ToPlayer(CCSPlayerController *controller);
