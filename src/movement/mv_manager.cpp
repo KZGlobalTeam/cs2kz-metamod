ff21cb3b2b786815a4a67f054ba31f8420213c8d
zer0.k <zer0kams@gmail.com>
2023-11-09 23:48:41 +0100

    Add assertions to ToPlayer

--- src/movement/mv_manager.cpp
+++ src/movement/mv_manager.cpp
@@ -6,28 +6,38 @@ extern CEntitySystem *g_pEntitySystem;
 
 MovementPlayer *CMovementPlayerManager::ToPlayer(CCSPlayer_MovementServices *ms)
 {
-	return this->players[ms->pawn->m_hController().GetEntryIndex()];
+	int index = ms->pawn->m_hController().GetEntryIndex();
+	assert(index >= 0 && index < MAXPLAYERS + 1);
+	return this->players[index];
 }
 
 MovementPlayer *CMovementPlayerManager::ToPlayer(CCSPlayerController *controller)
 {
-	return this->players[controller->m_pEntity->m_EHandle.GetEntryIndex()];
+	int index = controller->m_pEntity->m_EHandle.GetEntryIndex();
+	assert(index >= 0 && index < MAXPLAYERS + 1);
+	return this->players[index];
 }
 
 MovementPlayer *CMovementPlayerManager::ToPlayer(CBasePlayerPawn *pawn)
 {
-	return this->players[pawn->m_hController().GetEntryIndex()];
+	int index = pawn->m_hController().GetEntryIndex();
+	assert(index >= 0 && index < MAXPLAYERS + 1);
+	return this->players[index];
 }
 
 MovementPlayer *CMovementPlayerManager::ToPlayer(CPlayerSlot slot)
 {
-	return this->players[slot.Get() + 1];
+	int index = slot.Get() + 1;
+	assert(index >= 0 && index < MAXPLAYERS + 1);
+	return this->players[index];
 }
 
 MovementPlayer *CMovementPlayerManager::ToPlayer(CEntityIndex entIndex)
 {
 	if (!g_pEntitySystem) return nullptr;
-	return this->players[g_pEntitySystem->GetBaseEntity(entIndex)->m_pEntity->m_EHandle.GetEntryIndex()];
+	int index = g_pEntitySystem->GetBaseEntity(entIndex)->m_pEntity->m_EHandle.GetEntryIndex();
+	assert(index >= 0 && index < MAXPLAYERS + 1);
+	return this->players[index];
 }
 
 MovementPlayer *CMovementPlayerManager::ToPlayer(CPlayerUserId userID)
