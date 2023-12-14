1c2e4d9f43cd04f2b846289c24ff2210098e21b0
zer0.k <zer0kams@gmail.com>
2023-12-15 01:42:19 +0700

    Fix !hide crashes

--- src/kz/quiet/kz_quiet.cpp
+++ src/kz/quiet/kz_quiet.cpp
@@ -11,6 +11,18 @@ void KZ::quiet::OnCheckTransmit(CCheckTransmitInfo **pInfo, int infoCount)
 		CPlayerSlot targetSlot = pTransmitInfo->m_nClientEntityIndex;
 		KZPlayer *targetPlayer = KZ::GetKZPlayerManager()->ToPlayer(targetSlot);
 
+		// Never send dead players to prevent crashes.
+		for (int j = 0; j < MAXPLAYERS; j++)
+		{
+			if (j == targetPlayer->GetController()->entindex()) continue;
+			CBasePlayerPawn *pawn = KZ::GetKZPlayerManager()->players[j]->GetPawn();
+			if (!pawn) continue;
+			if (pTransmitInfo->m_pTransmitEdict->IsBitSet(pawn->entindex()) && pawn->m_lifeState() != LIFE_ALIVE)
+			{
+				pTransmitInfo->m_pTransmitEdict->Clear(pawn->entindex());
+			}
+		}
+
 		// Don't hide if player is dead/spectating or if they aren't hiding other players.
 		if (!targetPlayer->quietService->hideOtherPlayers) continue;
 		if (targetPlayer->GetPawn()->m_lifeState() != LIFE_ALIVE) continue;
