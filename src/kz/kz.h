559f27f096285b0e10b6888fb93de232fb6033e3
zer0.k <zer0kams@gmail.com>
2024-03-03 18:44:30 +0100

    Remove unused function declaration

--- src/kz/kz.h
+++ src/kz/kz.h
@@ -199,10 +199,6 @@ public:
 extern CKZPlayerManager *g_pKZPlayerManager;
 namespace KZ
 {
-	namespace HUD
-	{
-		void DrawSpeedPanel(KZPlayer *player);
-	}
 	namespace misc
 	{
 		void RegisterCommands();
