43f4489a061300d13b9a3d5f9570646e807ed7f2
LynchMus <xubw@vip.qq.com>
2024-03-05 08:57:59 +0000

    [skip ci] update.

--- src/kz/jumpstats/kz_jumpstats.cpp
+++ src/kz/jumpstats/kz_jumpstats.cpp
@@ -752,13 +752,15 @@ void KZJumpstatsService::PrintJumpToChat(KZPlayer *target, Jump *jump)
 	{
 		jumpColor = distanceTierColors[DistanceTier_Meh];
 	}
+	
 	/* MinTier broadcast TODO
 	if (GetTiers > DistanceTier_Perfect)
     {
         utils::CPrintChatAll("%s %s {grey}jumped %s%.1f {grey}units with a %s%s {grey}", KZ_CHAT_PREFIX, jump->GetJumpPlayer()->GetController()->m_iszPlayerName(), jumpColor, jump->GetDistance(), jumpColor, jumpTypeShortStr[jump->GetJumpType()]);
     }
 	*/
-	utils::CPrintChat(target->GetController(), "%s %s%s{grey}: %s%.1f {grey}| {olive}%i {grey}Strafes | {olive}%.0f%% {grey}Sync | {olive}%.2f {grey}Pre | {olive}%.2f {grey}Max\n\
+
+	jump->GetJumpPlayer()->PrintChat(true, true, "%s%s{grey}: %s%.1f {grey}| {olive}%i {grey}Strafes | {olive}%.0f%% {grey}Sync | {olive}%.2f {grey}Pre | {olive}%.2f {grey}Max\n\
 				{grey}BA {olive}%.0f%% {grey}| OL {olive}%.0f%% {grey}| DA {olive}%.0f%% {grey}| {olive}%.1f {grey}Deviation | {olive}%.1f {grey}Width | {olive}%.2f {grey}Height",
 		KZ_CHAT_PREFIX,
 		jumpColor,
