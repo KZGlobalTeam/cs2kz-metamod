caffc305d3e03b9a21457e16303a9dedf8ef87ed
zer0.k <zer0kams@gmail.com>
2024-03-11 03:04:37 +0100

    Update submodules, fix being able to start timer with perfs

--- src/kz/mode/kz_mode_ckz.cpp
+++ src/kz/mode/kz_mode_ckz.cpp
@@ -218,6 +218,7 @@ void KZClassicModeService::OnStopTouchGround()
 	// Perf
 	if (timeOnGround <= BH_PERF_WINDOW)
 	{
+		this->player->hitPerf = true;
 		// Perf speed
 		Vector2D landingVelocity2D(this->player->landingVelocity.x, this->player->landingVelocity.y);
 		landingVelocity2D.NormalizeInPlace();
