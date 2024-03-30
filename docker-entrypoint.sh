12177036596c0c460b8a8001ebc029d782a4cfd2
zer0.k <zer0kams@gmail.com>
2024-03-30 20:58:39 +0100

    Use clang16 to build on linux

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+apt update
+apt install clang-16
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-16 100
+update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-16 100
+
 cd build
 python3 ../configure.py
 ambuild
