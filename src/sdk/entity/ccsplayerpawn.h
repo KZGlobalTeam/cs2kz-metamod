3f1f220f7089d95fa88fded4d2eadb50dd613663
ipsvn <ipsvn@protonmail.com>
2024-04-26 01:58:44 +0100

    Move m_ignoreLadderJumpTime to CCSPlayerPawn

--- src/sdk/entity/ccsplayerpawn.h
+++ src/sdk/entity/ccsplayerpawn.h
@@ -10,7 +10,6 @@ public:
 	DECLARE_SCHEMA_CLASS(CCSPlayerPawnBase);
 	SCHEMA_FIELD(float, m_flSlopeDropOffset)
 	SCHEMA_FIELD(float, m_flSlopeDropHeight)
-	SCHEMA_FIELD(float, m_ignoreLadderJumpTime)
 	SCHEMA_FIELD(CCSPlayer_ViewModelServices*, m_pViewModelServices)
 };
 
@@ -18,6 +17,7 @@ class CCSPlayerPawn : public CCSPlayerPawnBase
 {
 public:
 	DECLARE_SCHEMA_CLASS(CCSPlayerPawn);
+	SCHEMA_FIELD(float, m_ignoreLadderJumpTime)
 
 	void Respawn() { CALL_VIRTUAL(void, g_pGameConfig->GetOffset("Respawn"), this); }
 };
