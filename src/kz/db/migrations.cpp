1d219df4b86b4c2881417cbfca02d8de3cab0e6a
zer0.k <zer0kams@gmail.com>
2024-08-24 03:56:26 +0200

    Another migration fix

--- src/kz/db/migrations.cpp
+++ src/kz/db/migrations.cpp
@@ -106,7 +106,7 @@ void KZDatabaseService::CheckMigrations(std::vector<ISQLQuery *> queries)
 	for (u32 i = 0; i < current; i++)
 	{
 		result->FetchRow();
-		u32 currentCRC = result->GetInt(1);
+		u32 currentCRC = result->GetInt64(1);
 		std::string migrationQuery;
 
 		switch (KZDatabaseService::GetDatabaseType())
