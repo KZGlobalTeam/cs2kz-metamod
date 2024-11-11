d5e7ef05cd5086132a89c98e6cf0ec51b0460288
GameChaos <franzkits@gmail.com>
2024-11-11 23:31:26 +0200

    Clang format moment

--- src/kz/kz_player.cpp
+++ src/kz/kz_player.cpp
@@ -726,7 +726,7 @@ void KZPlayer::DisableTurnbinds()
 	{
 		return;
 	}
-	
+
 	// holding both doesn't do anything. xor(1, 1) == 0
 	bool usingTurnbinds = (this->IsButtonPressed(IN_TURNLEFT) ^ this->IsButtonPressed(IN_TURNRIGHT));
 	QAngle angles;
