9e02992d71b0ef77a63589a3f734d86548be46be
zer0.k <61156310+zer0k-z@users.noreply.github.com>
2025-02-13 15:59:50 +0700

    Fix global cache not updating (#303)

--- src/kz/global/kz_global.cpp
+++ src/kz/global/kz_global.cpp
@@ -105,7 +105,7 @@ void KZGlobalService::Cleanup()
 
 void KZGlobalService::UpdateGlobalCache()
 {
-	if (KZGlobalService::currentMap.has_value())
+	if (!KZGlobalService::currentMap.has_value())
 	{
 		return;
 	}
