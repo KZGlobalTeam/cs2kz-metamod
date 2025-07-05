c9a3521fcbe5019dde808d67def669c104de50db
zer0.k <zer0kams@gmail.com>
2025-07-05 18:55:04 +0700

    Print out relevant triangles

--- src/sdk/physics/gamesystem.h
+++ src/sdk/physics/gamesystem.h
@@ -3,7 +3,7 @@
 
 class CPhysAggregateInstance
 {
-	u8 unknown[0x18];
+	uint8 unknown[0x18];
 	void *physicsWorld;
 
 public:
@@ -15,11 +15,11 @@ class CPhysicsGameSystem
 public:
 	struct PhysicsSpawnGroups_t
 	{
-		uint8_t unknown[0x10];
+		uint8 unknown[0x10];
 		CPhysAggregateInstance *m_pLevelAggregateInstance;
-		uint8_t unknown2[0x58];
+		uint8 unknown2[0x58];
 	};
 
-	uint8_t unk[0x78];
+	uint8 unk[0x78];
 	CUtlMap<uint, PhysicsSpawnGroups_t> m_PhysicsSpawnGroups;
 };
