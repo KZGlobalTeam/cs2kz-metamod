c782df5735843413abeaa68c3a0ef0fd87b29e02
zer0.k <zer0kams@gmail.com>
2025-08-09 16:18:04 +0200

    Fix particle system crashes

--- src/kz/beam/kz_beam.cpp
+++ src/kz/beam/kz_beam.cpp
@@ -173,7 +173,6 @@ void KZBeamService::UpdatePlayerBeam()
 
 		beam->DispatchSpawn(pKeyValues);
 		this->playerBeam = beam->GetRefEHandle();
-		delete pKeyValues;
 	}
 	origin += this->playerBeamOffset;
 	beam->Teleport(&origin, nullptr, &vec3_origin);
@@ -193,8 +192,6 @@ void KZBeamService::UpdatePlayerBeam()
 			newBeam->DispatchSpawn(pKeyValues);
 			newBeam->m_iTeamNum(CUSTOM_PARTICLE_SYSTEM_TEAM);
 			this->playerBeamNew = newBeam->GetRefEHandle();
-
-			delete pKeyValues;
 		}
 		else if (g_pKZUtils->GetServerGlobals()->curtime - beam->m_flStartTime().GetTime() > 3.2f)
 		{
