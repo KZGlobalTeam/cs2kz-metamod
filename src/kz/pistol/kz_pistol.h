54548a5c4425ee6b4976931ff5410f7f5db123db
zer0.k <zer0kams@gmail.com>
2025-09-14 05:40:21 +0200

    Add more gun aliases

--- src/kz/pistol/kz_pistol.h
+++ src/kz/pistol/kz_pistol.h
@@ -14,17 +14,17 @@ class KZPistolService : public KZBaseService
 {
 public:
 	static inline const std::vector<PistolInfo_t> pistols = {
-		{-1, CS_TEAM_NONE, "Disabled", "", {"knife", "disable", "disabled", "none", "0"}},
-		{1, CS_TEAM_NONE, "Desert Eagle", "weapon_deagle", {"deagle", "de", "deag"}},
-		{2, CS_TEAM_NONE, "Dual Berettas", "weapon_elite", {"elite", "dualies", "dual berettas"}},
-		{3, CS_TEAM_CT, "Five-SeveN", "weapon_fiveseven", {"fiveseven", "57"}},
-		{4, CS_TEAM_T, "Glock-18", "weapon_glock", {"glock", "glock18"}},
-		{30, CS_TEAM_T, "Tec-9", "weapon_tec9", {"tec9"}},
-		{32, CS_TEAM_CT, "P2000", "weapon_hkp2000", {"p2000"}},
+		{-1, CS_TEAM_NONE, "Disabled", "", {"knife", "disable", "disabled", "none", "off", "0"}},
+		{1, CS_TEAM_NONE, "Desert Eagle", "weapon_deagle", {"deagle", "deag", "desert_eagle", "deserteagle", "desert-eagle", "de"}},
+		{2, CS_TEAM_NONE, "Dual Berettas", "weapon_elite", {"elite", "dualies", "dual_elites", "dual_berettas", "dual-elites", "dual-berettas", "dualelites", "dualberettas", "berettas", "dual berettas"}},
+		{3, CS_TEAM_CT, "Five-SeveN", "weapon_fiveseven", {"fiveseven", "five_seven", "five7", "five_7", "five-7", "5seven", "5_seven", "5-seven", "57", "5-7", "5_7"}},
+		{4, CS_TEAM_T, "Glock-18", "weapon_glock", {"glock", "glock18", "glock_18", "glock-18"}},
+		{30, CS_TEAM_T, "Tec-9", "weapon_tec9", {"tec9", "tec-9", "tec_9"}},
+		{32, CS_TEAM_CT, "P2000", "weapon_hkp2000", {"hkp2000", "p2k", "p2000"}},
 		{36, CS_TEAM_NONE, "P250", "weapon_p250", {"p250"}},
-		{61, CS_TEAM_CT, "USP-S", "weapon_usp_silencer", {"usps", "usp-s", "usp"}},
-		{63, CS_TEAM_NONE, "CZ75-Auto", "weapon_cz75a", {"cz75-auto", "cs75a", "cz"}},
-		{64, CS_TEAM_NONE, "R8 Revolver", "weapon_revolver", {"r8 revolver", "r8revolver", "revolver", "r8"}}};
+		{61, CS_TEAM_CT, "USP-S", "weapon_usp_silencer", {"usps", "usp-s", "usp"}}, 
+		{63, CS_TEAM_NONE, "CZ75-Auto", "weapon_cz75a", {"cz75-auto", "cz75a", "cz75", "cz-75", "cz_75", "cz", "cz75_auto", "cz-75_auto", "cz_75_auto"}},
+		{64, CS_TEAM_NONE, "R8 Revolver", "weapon_revolver", {"r8 revolver", "r8revolver", "revolver", "r8", "r8_revolver"}}};
 
 	static int GetPistolIndexByName(const char *name)
 	{
