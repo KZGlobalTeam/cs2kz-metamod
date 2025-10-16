fb93bf2d137cef4cd49e17bdec5751c771b75f47
zer0.k <zer0kams@gmail.com>
2025-10-16 05:17:33 +0200

    Update emit sound structs, fix offsets and sigs, update sdk for 2025-10-15 update

--- src/sdk/datatypes.h
+++ src/sdk/datatypes.h
@@ -72,73 +72,41 @@ struct SndOpEventGuid_t
 
 	SoundEventGuid_t m_nGuid;
 	uint32 m_hStackHash;
-	uint32 unknown[3];
+	// #define SND_OP_FLAG_DEFAULT (1 << 0)
+	// #define SND_OP_FLAG_RELIABLE (1 << 1)
+	// #define SND_OP_FLAG_INIT (1 << 2)
+	// #define SND_OP_FLAG_VOICE (1 << 3)
+	uint8 m_nFlags;
+	CPlayerBitVec m_Players;
 };
 
-// used with EmitSound_t
-enum gender_t : uint8
-{
-	GENDER_NONE = 0x0,
-	GENDER_MALE = 0x1,
-	GENDER_FEMALE = 0x2,
-	GENDER_NAMVET = 0x3,
-	GENDER_TEENGIRL = 0x4,
-	GENDER_BIKER = 0x5,
-	GENDER_MANAGER = 0x6,
-	GENDER_GAMBLER = 0x7,
-	GENDER_PRODUCER = 0x8,
-	GENDER_COACH = 0x9,
-	GENDER_MECHANIC = 0xA,
-	GENDER_CEDA = 0xB,
-	GENDER_CRAWLER = 0xC,
-	GENDER_UNDISTRACTABLE = 0xD,
-	GENDER_FALLEN = 0xE,
-	GENDER_RIOT_CONTROL = 0xF,
-	GENDER_CLOWN = 0x10,
-	GENDER_JIMMY = 0x11,
-	GENDER_HOSPITAL_PATIENT = 0x12,
-	GENDER_BRIDE = 0x13,
-	GENDER_LAST = 0x14,
-};
-
-struct EmitSound_t // Need update, this just fix crash
+struct EmitSound_t
 {
 	// clang-format off
 	EmitSound_t() :
 		m_pSoundName( 0 ),
 		m_flVolume( VOL_NORM ),
-		m_SoundLevel( SNDLVL_NONE ),
-		m_nFlags( 0 ),
-		m_nPitch( PITCH_NORM ),
-		m_pOrigin( 0 ),
 		m_flSoundTime( 0.0f ),
-		m_pflSoundDuration( 0 ),
-		m_bEmitCloseCaption( true ),
-		m_bWarnOnMissingCloseCaption( false ),
-		m_bWarnOnDirectWaveReference( false ),
 		m_nSpeakerEntity( -1 ),
-		m_UtlVecSoundOrigin(),
 		m_nForceGuid( 0 ),
-		m_SpeakerGender( GENDER_NONE )
+		m_nSourceSoundscape( 0 ),
+		m_nPitch( PITCH_NORM )
 	{
 	}
 
 	// clang-format on
 	const char *m_pSoundName;
-	float m_flVolume;
-	soundlevel_t m_SoundLevel;
-	int m_nFlags;
-	int m_nPitch;
-	const Vector *m_pOrigin;
-	float m_flSoundTime;
-	float *m_pflSoundDuration;
-	bool m_bEmitCloseCaption;
-	bool m_bWarnOnMissingCloseCaption;
-	bool m_bWarnOnDirectWaveReference;
+	Vector m_vecOrigin;
+	float m_flVolume;    // soundevent's volume_atten
+	float m_flSoundTime; // sound delay
 	CEntityIndex m_nSpeakerEntity;
-	CUtlVector<Vector, CUtlMemory<Vector, int>> m_UtlVecSoundOrigin;
 	SoundEventGuid_t m_nForceGuid;
-	gender_t m_SpeakerGender;
+	CEntityIndex m_nSourceSoundscape;
+	uint16 m_nPitch; // Pretty sure this is unused.
+	// (1<<3) overrides the source soundscape with the speaker entity.
+	// (1<<4) emits sound at specified position, otherwise attached to entity index.
+	// Possibly share the same flags as SndOpEventGuid_t.
+	uint8 m_nFlags;
 };
 
 // Tracing stuff.
@@ -188,6 +156,7 @@ public:
 		m_flUpMove {source.m_flUpMove},
 		m_vecVelocity {source.m_vecVelocity}, 
 		m_vecAngles {source.m_vecAngles},
+		m_vecUnknown {source.m_vecUnknown},
 		m_bHasSubtickInputs {source.m_bHasSubtickInputs},
 		unknown {source.unknown},
 		m_collisionNormal {source.m_collisionNormal},
@@ -242,6 +211,7 @@ public:
 	float m_flUpMove;
 	Vector m_vecVelocity;
 	QAngle m_vecAngles;
+	Vector m_vecUnknown;
 	CUtlVector<SubtickMove> m_SubtickMoves;
 	CUtlVector<SubtickMove> m_AttackSubtickMoves;
 	bool m_bHasSubtickInputs;
@@ -278,7 +248,7 @@ public:
 	bool m_bGameCodeMovedPlayer; // true if usercmd cmd number == (m_nGameCodeHasMovedPlayerAfterCommand + 1)
 };
 
-static_assert(sizeof(CMoveData) == 280, "Class didn't match expected size");
+static_assert(sizeof(CMoveData) == 296, "Class didn't match expected size");
 
 // Custom data types goes here.
 
