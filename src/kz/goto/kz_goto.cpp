6aed76610c7173acb12cff0a1d26cffb9fbfaf25
zer0.k <zer0kams@gmail.com>
2026-03-04 13:26:41 +0700

    Prioritize exact name match for !goto

--- src/kz/goto/kz_goto.cpp
+++ src/kz/goto/kz_goto.cpp
@@ -26,6 +26,9 @@ bool KZGotoService::GotoPlayer(const char *playerNamePart)
 		return false;
 	}
 
+	KZPlayer *targetPlayer = nullptr;
+
+	// Prefer exact matches over partial matches.
 	for (i32 i = 0; i <= MAXPLAYERS; i++)
 	{
 		CBasePlayerController *controller = g_pKZPlayerManager->players[i]->GetController();
@@ -36,50 +39,79 @@ bool KZGotoService::GotoPlayer(const char *playerNamePart)
 			continue;
 		}
 
-		if (V_strstr(V_strlower((char *)otherPlayer->GetName()), V_strlower((char *)playerNamePart)))
+		if (KZ_STREQI(otherPlayer->GetName(), playerNamePart))
 		{
 			if (otherPlayer->GetController()->GetTeam() == CS_TEAM_SPECTATOR)
 			{
-				this->player->languageService->PrintChat(true, false, "Goto - Error Message (Player In Spec)", otherPlayer->GetName());
-				return false;
+				continue;
 			}
-
-			if (this->player->GetController()->GetTeam() == CS_TEAM_SPECTATOR)
-			{
-				this->player->GetController()->SwitchTeam(CS_TEAM_CT);
-				this->player->GetController()->Respawn();
-			}
-
-			CCSPlayer_MovementServices *ms = this->player->GetMoveServices();
-
-			if (otherPlayer->GetMoveType() == MOVETYPE_LADDER)
-			{
-				ms->m_vecLadderNormal(otherPlayer->GetMoveServices()->m_vecLadderNormal());
-				this->player->SetMoveType(MOVETYPE_LADDER);
-			}
-			else
-			{
-				ms->m_vecLadderNormal(vec3_origin);
-			}
-
-			Vector origin;
-			QAngle angles;
-			otherPlayer->GetOrigin(&origin);
-			otherPlayer->GetAngles(&angles);
-
-			this->player->GetPlayerPawn()->Teleport(&origin, &angles, &NULL_VECTOR);
-			this->player->languageService->PrintChat(true, false, "Goto - Teleported", otherPlayer->GetName());
-			if (this->player->GetPlayerPawn()->m_Collision().m_CollisionGroup() != KZ_COLLISION_GROUP_STANDARD)
-			{
-				this->player->GetPlayerPawn()->m_Collision().m_CollisionGroup() = KZ_COLLISION_GROUP_STANDARD;
-				this->player->GetPlayerPawn()->CollisionRulesChanged();
-			}
-			return true;
+			targetPlayer = otherPlayer;
+			break;
 		}
 	}
 
-	player->languageService->PrintChat(true, false, "Error Message (Player Not Found)", playerNamePart);
-	return false;
+	// If no exact match was found, try partial matches.
+	if (!targetPlayer)
+	{
+		for (i32 i = 0; i <= MAXPLAYERS; i++)
+		{
+			CBasePlayerController *controller = g_pKZPlayerManager->players[i]->GetController();
+			KZPlayer *otherPlayer = g_pKZPlayerManager->ToPlayer(i);
+
+			if (!controller || this->player == otherPlayer)
+			{
+				continue;
+			}
+
+			if (V_strstr(V_strlower((char *)otherPlayer->GetName()), V_strlower((char *)playerNamePart)))
+			{
+				if (otherPlayer->GetController()->GetTeam() == CS_TEAM_SPECTATOR)
+				{
+					continue;
+				}
+				targetPlayer = otherPlayer;
+				break;
+			}
+		}
+	}
+
+	if (!targetPlayer)
+	{
+		player->languageService->PrintChat(true, false, "Error Message (Player Not Found)", playerNamePart);
+		return false;
+	}
+
+	if (this->player->GetController()->GetTeam() == CS_TEAM_SPECTATOR)
+	{
+		this->player->GetController()->SwitchTeam(CS_TEAM_CT);
+		this->player->GetController()->Respawn();
+	}
+
+	CCSPlayer_MovementServices *ms = this->player->GetMoveServices();
+
+	if (targetPlayer->GetMoveType() == MOVETYPE_LADDER)
+	{
+		ms->m_vecLadderNormal(targetPlayer->GetMoveServices()->m_vecLadderNormal());
+		this->player->SetMoveType(MOVETYPE_LADDER);
+	}
+	else
+	{
+		ms->m_vecLadderNormal(vec3_origin);
+	}
+
+	Vector origin;
+	QAngle angles;
+	targetPlayer->GetOrigin(&origin);
+	targetPlayer->GetAngles(&angles);
+
+	this->player->GetPlayerPawn()->Teleport(&origin, &angles, &NULL_VECTOR);
+	this->player->languageService->PrintChat(true, false, "Goto - Teleported", targetPlayer->GetName());
+	if (this->player->GetPlayerPawn()->m_Collision().m_CollisionGroup() != KZ_COLLISION_GROUP_STANDARD)
+	{
+		this->player->GetPlayerPawn()->m_Collision().m_CollisionGroup() = KZ_COLLISION_GROUP_STANDARD;
+		this->player->GetPlayerPawn()->CollisionRulesChanged();
+	}
+	return true;
 }
 
 SCMD(kz_goto, SCFL_PLAYER)
