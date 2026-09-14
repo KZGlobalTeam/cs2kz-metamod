d2be31c4d8cdde1196448f55c606ffec694699ff
Dots <dotvnl@gmail.com>
2026-09-14 23:08:22 +0300

    only reset clantags cs2kz set

--- src/kz/profile/kz_profile.cpp
+++ src/kz/profile/kz_profile.cpp
@@ -237,7 +237,8 @@ void KZProfileService::UpdateClantag()
 	}
 	if (!kz_profile_clantag_enabled.Get())
 	{
-		if (this->player->GetController() && V_strlen(this->player->GetController()->m_szClan().String()) > 0)
+		if (this->player->GetController() && this->clanTag[0] != '\0'
+			&& V_strcmp(this->player->GetController()->m_szClan().String(), this->clanTag) == 0)
 		{
 			this->SetClantag("");
 		}
